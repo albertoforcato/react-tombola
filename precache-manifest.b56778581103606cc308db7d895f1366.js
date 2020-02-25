f5ba3258808943be65bf0966ced71c90c16d5733
albertoforcato <45015176+albertoforcato@users.noreply.github.com>
2020-02-25 17:07:39 +0100

    Updates

---
+++ precache-manifest.b56778581103606cc308db7d895f1366.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403e7ea059e0fa5ddf972238e74523a2",
+    "url": "/tombola-react/index.html"
+  },
+  {
+    "revision": "c2c65bd15603cce054d1",
+    "url": "/tombola-react/static/css/main.ccc70576.chunk.css"
+  },
+  {
+    "revision": "cb4f750db10ed2c1ef3c",
+    "url": "/tombola-react/static/js/2.5fbcba4c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/tombola-react/static/js/2.5fbcba4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c65bd15603cce054d1",
+    "url": "/tombola-react/static/js/main.904a06f4.chunk.js"
+  },
+  {
+    "revision": "d5e11e9c63b85146c246",
+    "url": "/tombola-react/static/js/runtime-main.1a863a26.js"
+  }
+]);
