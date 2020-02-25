4c6a3d27a2e79e2f7d17d003c7b2df31769e25d0
albertoforcato <45015176+albertoforcato@users.noreply.github.com>
2020-02-25 17:42:21 +0100

    Updates

---
+++ precache-manifest.96fe4f120af418e673409d2565d6c755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9898790c39678bdc61bc87386d8ac466",
+    "url": "/react-tombola/index.html"
+  },
+  {
+    "revision": "eac98c57e50faf9f5889",
+    "url": "/react-tombola/static/css/main.a7a122c8.chunk.css"
+  },
+  {
+    "revision": "d3f759d7be1b8690bcbc",
+    "url": "/react-tombola/static/js/2.2e4ac991.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-tombola/static/js/2.2e4ac991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac98c57e50faf9f5889",
+    "url": "/react-tombola/static/js/main.9300607b.chunk.js"
+  },
+  {
+    "revision": "b639d0bb560b832df31e",
+    "url": "/react-tombola/static/js/runtime-main.821cdeeb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-tombola/precache-manifest.b2727b3016b97aa0e66a356e2430a156.js"
+  "/react-tombola/precache-manifest.96fe4f120af418e673409d2565d6c755.js"
 );
 
 self.addEventListener('message', (event) => {
