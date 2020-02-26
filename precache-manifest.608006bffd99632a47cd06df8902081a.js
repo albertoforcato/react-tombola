e85759cac2044bfbad01f4b91df2d1c6c2154ec8
albertoforcato <45015176+albertoforcato@users.noreply.github.com>
2020-02-26 18:37:45 +0100

    Updates

--- precache-manifest.608006bffd99632a47cd06df8902081a.js
+++ precache-manifest.608006bffd99632a47cd06df8902081a.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d45950d368da736b221e04cba16d9e39",
+    "revision": "5822e79e5c0ead0c3198fe79d0497480",
     "url": "/react-tombola/index.html"
   },
   {
-    "revision": "a6275d2c7ce88525f43d",
+    "revision": "cea75a5ead6778b452e8",
     "url": "/react-tombola/static/css/2.67123b20.chunk.css"
   },
   {
-    "revision": "fe8e20e663c0dce8c2ce",
+    "revision": "0b102a3f6c1f131dc4fc",
     "url": "/react-tombola/static/css/main.a7a122c8.chunk.css"
   },
   {
-    "revision": "a6275d2c7ce88525f43d",
-    "url": "/react-tombola/static/js/2.311ea8d1.chunk.js"
+    "revision": "cea75a5ead6778b452e8",
+    "url": "/react-tombola/static/js/2.e5473c9f.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/react-tombola/static/js/2.311ea8d1.chunk.js.LICENSE.txt"
+    "url": "/react-tombola/static/js/2.e5473c9f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fe8e20e663c0dce8c2ce",
-    "url": "/react-tombola/static/js/main.0782f27b.chunk.js"
+    "revision": "0b102a3f6c1f131dc4fc",
+    "url": "/react-tombola/static/js/main.a6492454.chunk.js"
   },
   {
     "revision": "b639d0bb560b832df31e",
