a2b07bb0ed2484a1c2f1dd3c612be8bb8b3341e6
albertoforcato <45015176+albertoforcato@users.noreply.github.com>
2020-03-02 14:28:17 +0100

    Deleted WrapSection

--- src/wrap-components/WrapSection.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const WrapSection = ({ className, children }) => (
-  <section className="container-fluid">
-    <section className="row justify-content-center">{children}</section>
-  </section>
-);
-
-export default WrapSection;
